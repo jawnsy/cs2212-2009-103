9e2b2e93b93df206e86f5b960ece68608d87edc0
jlu.newera <jlu.newera@e8c3db4c-ebf1-11dd-bce9-bf72c0239d3a>
2009-03-04 20:37:38 +0000

    add empty GarageSale class update class description for Printer, GarageSaleRank class.

---
+++ trunk/src/ca/uwo/garage/storage/GarageSale.java
@@ -0,0 +1,5 @@
+package ca.uwo.garage.storage;
+
+public class GarageSale {
+
+}
--- trunk/src/ca/uwo/garage/storage/Printer.java
+++ trunk/src/ca/uwo/garage/storage/Printer.java
@@ -2,10 +2,28 @@ package ca.uwo.garage.storage;
 
 import java.util.Collection;
 
+/**
+ * this interface is used to provide method to print
+ * the garage sale info into HTML format
+ */
+
 public interface Printer {
-	
+//HELPER METHOD****************************************
+	/**
+	 * this print method print a single garage sale information
+	 * into HTML format.
+	 * @param garageSale the GarageSale object to be printed out
+	 * @throws PrinterException 
+	 */
 	void print(GarageSale garageSale)
 		throws PrinterException;
+	
+	/**
+	 * this print method print a collection of garage sales information
+	 * into HTML format
+	 * @param sales Collection of GarageSale objects
+	 * @throws PrinterException
+	 */
 	void print(Collection sales)
 		throws PrinterException;
 
