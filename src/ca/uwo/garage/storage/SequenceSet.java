84d97852fbc4f2d5ffbaa964d1050129c9a8b7e2
jonathan.i.yu <jonathan.i.yu@e8c3db4c-ebf1-11dd-bce9-bf72c0239d3a>
2009-03-07 22:05:49 +0000

    - Added some documentation - Added some skeleton code for a GarageSale - TODO: Finish Storage of GarageSales -- the code is broken for now.

--- src/ca/uwo/garage/storage/SequenceSet.java
+++ src/ca/uwo/garage/storage/SequenceSet.java
@@ -13,15 +13,37 @@ public class SequenceSet
 
 	private transient TreeMap<String, Sequence> m_sequences;
 
+	/**
+	 * This creates a new SequenceSet, which is a set of numeric Sequence
+	 * objects that are keyed by a unique id String. This replicates some of
+	 * the basic ideas behind a Relational Database's Auto-incrementing
+	 * Primary Key system or the sequence generator available in some dialects.
+	 * 
+	 * It is designed to provide the next number in a sequence suitable for use in
+	 * generating a unique, non-reusable id, beginning at 0.
+	 */
 	public SequenceSet() {
 		m_sequences = new TreeMap<String, Sequence>();
 	}
 
+	/**
+	 * This registers a certain key. When an object wants to make use of a given
+	 * sequence name, it must first register the name of the sequence using this
+	 * method.
+	 * 
+	 * If a sequence name is registered more than once, then a warning is printed
+	 * to the Standard Error socket -- while not a fatal error, only one part of
+	 * the code should be using each sequence name. This registration therefore
+	 * prevents strange action-at-a-distance type bugs.
+	 * 
+	 * @param key The sequence name (key string)
+	 */
 	public void register(String key) {
 		Sequence seq = null;
 
-		// If the sequence is already loaded, grab it and check if it has been already registered;
-		// emit a warning to STDERR if it has
+		/* If the sequence is already loaded, grab it and check if it has been
+		 * already registered; emit a warning to STDERR if it has
+		 */
 		if (m_sequences.containsKey(key)) {
 			seq = m_sequences.get(key);
 			if (seq.registered())
@@ -36,6 +58,15 @@ public class SequenceSet
 		// seq is guaranteed to exist now; register it
 		seq.register();
 	}
+
+	/**
+	 * Gets the next unique value in the sequence. Sequence numbers begin at 0 and are
+	 * incremented every time nextval() is called. Sequence numbers are guaranteed to
+	 * be unique and will never be reused.
+	 * 
+	 * @param key The sequence's unique key name
+	 * @return The next value in the sequence
+	 */
 	public int nextval(String key) {
 		Sequence seq = null;
 
@@ -54,6 +85,7 @@ public class SequenceSet
 		return seq.nextval();
 	}
 
+	// Internal code for storing and restoring the object
 	private void writeObject(ObjectOutputStream out)
 		throws IOException
 	{
@@ -68,7 +100,6 @@ public class SequenceSet
 			out.writeInt(m_sequences.get(key).value());
 		}
 	}
-
 	private void readObject(ObjectInputStream in)
 		throws IOException, ClassNotFoundException
 	{
@@ -81,4 +112,5 @@ public class SequenceSet
 			m_sequences.put(key, seq);
 		}
 	}
+
 }
--- src/ca/uwo/garage/storage/Storage.java
+++ src/ca/uwo/garage/storage/Storage.java
@@ -3,17 +3,58 @@ import java.util.Collection;
 
 public interface Storage
 {
+	/**
+	 * If the Storage medium is empty, then return true.
+	 * 
+	 * @return true if the Storage is empty
+	 */
 	public abstract boolean isEmpty();
+
+	/**
+	 * If the Storage medium is at its capacity, then return true.
+	 * 
+	 * @return true if the Storage is full
+	 */
 	public abstract boolean isFull();
 
+	/**
+	 * @return The number of objects in our storage
+	 */
 	public abstract int size();
+
+	/**
+	 * This provides some Storage-specific notion of its length.
+	 * It could be, for example, the size of the storage on disk,
+	 * but that may not be the case.
+	 * 
+	 * @return a Storage implementation-specific length
+	 */
 	public abstract long length();
 
+	/**
+	 * This initiates a connection to the backing database.
+	 * 
+	 * @throws StorageException if there is an error connecting
+	 */
 	public abstract void connect()
 		throws StorageException;
+
+	/**
+	 * This closes the connection to the database when done, saving any
+	 * data if necessary.
+	 * 
+	 * @throws StorageException if there is an failure
+	 */
 	public abstract void disconnect()
 		throws StorageException;
 
+	/**
+	 * This tries to find a user given their unique userid string.
+	 * 
+	 * @param userid The User's userid string
+	 * @return a User object with a matching userid
+	 * @throws StorageNotFoundException if the specific userid cannot be found
+	 */
 	public abstract User findUser(String userid)
 		throws StorageNotFoundException;
 	public abstract Collection<User> listUsers()
@@ -33,4 +74,14 @@ public interface Storage
 	public abstract void delete(Category user)
 		throws StorageNotFoundException;
 	public abstract boolean existsCategory(int categoryid);
+
+	public abstract GarageSale findGarageSale(int saleid)
+		throws StorageNotFoundException;
+	public abstract Collection<GarageSale> listGarageSales()
+		throws StorageEmptyException;
+	public abstract void store(GarageSale sale)
+		throws StorageFullException, StorageKeyException;
+	public abstract void delete(GarageSale sale)
+		throws StorageNotFoundException;
+	public abstract boolean existsSale(int saleid);	
 }
