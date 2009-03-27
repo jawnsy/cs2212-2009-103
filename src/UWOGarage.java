167f6c54ec17296ce887b3b388275853bb61825d
jonathan.i.yu <jonathan.i.yu@e8c3db4c-ebf1-11dd-bce9-bf72c0239d3a>
2009-03-27 22:26:27 +0000

    - Added svn:keyword properties

--- src/UWOGarage.java
+++ src/UWOGarage.java
@@ -1,11 +1,13 @@
-import ca.uwo.garage.AdminController;
-import ca.uwo.garage.AdminView;
+import ca.uwo.garage.AuthorizationController;
+import ca.uwo.garage.AuthorizationView;
 import ca.uwo.garage.Controller;
 import ca.uwo.garage.View;
 
 public class UWOGarage {
 	public static void main(String[] args) {
-		Controller control = new AdminController();
-		View view = new AdminView(control);
+		//Controller control = new AdminController();
+		//View view = new AdminView(control);
+		Controller control = new AuthorizationController();
+		View view = new AuthorizationView(control);
 	}
 }
