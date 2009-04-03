9fd026dc0dfa8aa581083bd7f42a275aa44a3cda
nighthawk8p@hotmail.com <nighthawk8p@hotmail.com@e8c3db4c-ebf1-11dd-bce9-bf72c0239d3a>
2009-04-03 02:12:55 +0000

    javadoc

--- trunk/src/ca/uwo/garage/storage/GarageSaleLoaderException.java
+++ trunk/src/ca/uwo/garage/storage/GarageSaleLoaderException.java
@@ -1,3 +1,9 @@
+/**This class represents an exception that our 
+ * program would want to catch
+ * @author CS2212 Group 103
+ * @extends GarageException
+ */
+
 package ca.uwo.garage.storage;
 
 import ca.uwo.garage.GarageException;
@@ -6,10 +12,17 @@ import ca.uwo.garage.GarageException;
 public class GarageSaleLoaderException
 	extends GarageException
 {
+	/** Constructs a new exception to be thrown if anything happens
+	 *  in GarageSaleLoader while bulk loading in the data
+ 	 */
 	public GarageSaleLoaderException()
 	{
 		super("Failed to load new or save imported GarageSales");
 	}
+	/**Constructs a new exception with the specified 
+	 * detail message
+	 * @param msg
+	 */
 	public GarageSaleLoaderException(String msg)
 	{
 		super(msg);
